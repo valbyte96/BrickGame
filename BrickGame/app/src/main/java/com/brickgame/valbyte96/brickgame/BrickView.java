3b19de3d888daddd94175d088730237e30ce15c6
valbyte96 <vmcculloch@smith.edu>
2017-01-17 14:16:45 -0500

    Fixed xml

--- BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/BrickView.java
+++ BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/BrickView.java
@@ -1,6 +1,12 @@
 package com.brickgame.valbyte96.brickgame;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,20 +17,51 @@ import android.view.View;
 
 public class BrickView extends View {
 
-    BrickView(Context context){
-        super(context);
-       // init();
+    private Paint bgPaint;
 
+
+    public BrickView(Context context) {
+        super(context);
+        init();
     }
 
-   public void init(){
+    public BrickView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BrickView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public BrickView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init(){
+        bgPaint = new Paint();
+        bgPaint.setColor(Color.rgb(0,0,0));
        setOnTouchListener(new OnTouchListener() {
            @Override
-           public boolean onTouch(View v, MotionEvent event) {
-               Log.d("test",Float.toString(event.getX()));
+           public boolean onTouch(View v, MotionEvent motionEvent) {
+               if (motionEvent.getActionMasked() == MotionEvent.ACTION_DOWN) {
+                   return true;
+               }
+               if (motionEvent.getActionMasked() == MotionEvent.ACTION_MOVE) {
+                   Log.d("test", "x: " + motionEvent.getX() + ", y: " + motionEvent.getY());
+                   return true;
+               }
                return false;
            }
          }
        );
    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawRect(0,0,getWidth(),getHeight(),bgPaint);
+    }
 }
