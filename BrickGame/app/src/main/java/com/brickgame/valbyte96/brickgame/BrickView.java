4cc8a6002c3f13773d7871f7cb84c88f6cdfe148
valbyte96 <vmcculloch@smith.edu>
2017-01-17 11:38:02 -0500

    Some bar implementation

--- BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/BrickView.java
+++ BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/BrickView.java
@@ -1,6 +1,8 @@
 package com.brickgame.valbyte96.brickgame;
 
 import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -8,7 +10,21 @@ import android.view.View;
  */
 
 public class BrickView extends View {
+
     BrickView(Context context){
         super(context);
+       // init();
+
     }
+
+   public void init(){
+       setOnTouchListener(new OnTouchListener() {
+           @Override
+           public boolean onTouch(View v, MotionEvent event) {
+               Log.d("test",Float.toString(event.getX()));
+               return false;
+           }
+         }
+       );
+   }
 }
