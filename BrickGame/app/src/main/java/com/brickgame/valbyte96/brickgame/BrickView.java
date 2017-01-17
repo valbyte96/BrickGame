e92e3341dd703f06553b8a75e32558ff1f941974
valbyte96 <vmcculloch@smith.edu>
2017-01-17 10:46:44 -0500

    Basic Classes

---
+++ BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/BrickView.java
@@ -0,0 +1,14 @@
+package com.brickgame.valbyte96.brickgame;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by demouser on 1/17/17.
+ */
+
+public class BrickView extends View {
+    BrickView(Context context){
+        super(context);
+    }
+}
