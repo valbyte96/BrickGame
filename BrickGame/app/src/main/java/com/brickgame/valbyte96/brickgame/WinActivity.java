b1259a48312c26e52f0a1e0c4a66786aa62f6230
Demo User <demouser@MacBook-Air.local>
2017-01-20 11:00:39 -0500

    changes

--- BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/WinActivity.java
+++ BrickGame/app/src/main/java/com/brickgame/valbyte96/brickgame/WinActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class WinActivity extends AppCompatActivity {
 
@@ -12,6 +13,14 @@ public class WinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+
+        TextView textView1 = (TextView) findViewById(R.id.userName);
+        if(username != null){
+            textView1.setText(username);
+        }
 
         ((Button) findViewById(R.id.restartButton)).setOnClickListener(new View.OnClickListener() {
             @Override
